16f86639ad6ac3ce384176994d3a6b40d96485a5
James Coglan <jcoglan@gmail.com>
2012-06-17 11:15:07 +0100

    Compare stats for Vault againsts a randomized generator.

--- spec/node/random.js
+++ spec/node/random.js
@@ -2,38 +2,51 @@ function rand(n) {
   return String(Math.random() * Math.pow(2,48));
 }
 
+function test(generator) {
+  var res = {},
+      N   = 2000,
+      pass, chars, i, n, j;
+  
+  while (N--) {
+    pass  = generator.generate(rand());
+    chars = pass.split('');
+    for (i = 0, n = chars.length; i < n; i++) {
+      j = Vault.ALL.indexOf(chars[i]);
+      res[j] = res[j] || 0;
+      res[j] += 1;
+    }
+  }
+  
+  var sum    = 0,
+      square = 0,
+      n      = Vault.ALL.length;
+  
+  while (n--) {
+    sum    += res[n];
+    square += Math.pow(res[n], 2);
+  }
+  
+  var mean   = sum / Vault.ALL.length,
+      stddev = Math.sqrt(square / Vault.ALL.length - Math.pow(mean, 2));
+  
+  console.log('Mean:     ' + mean);
+  console.log('Std.dev.: ' + stddev);
+}
+
 var Vault = require('../../lib/vault'),
     vault = new Vault({phrase: rand()});
 
-var res = {},
-    N   = 10000,
-    pass, chars, i, n, j;
-
-while (N--) {
-  pass  = vault.generate(rand());
-  chars = pass.split('');
-  for (i = 0, n = chars.length; i < n; i++) {
-    j = Vault.ALL.indexOf(chars[i]);
-    res[j] = res[j] || 0;
-    res[j] += 1;
+var random = {
+  generate: function() {
+    var string = '';
+    while (string.length < Vault.DEFAULT_LENGTH)
+      string += Vault.ALL[Math.floor(Math.random() * Vault.ALL.length)];
+    return string;
   }
-}
+};
 
-console.log(res);
+console.log('Random generator:')
+test(random);
 
-var sum    = 0,
-    square = 0,
-    n      = Vault.ALL.length;
-
-while (n--) {
-  sum    += res[n];
-  square += Math.pow(res[n], 2);
-}
-
-var mean   = sum / Vault.ALL.length,
-    stddev = Math.sqrt(square / Vault.ALL.length - Math.pow(mean, 2));
-
-console.log('Mean:     ' + mean);
-console.log('Expected: ' + (10000*20/94));
-console.log('Std.dev.: ' + stddev);
-
+console.log('\nVault:')
+test(vault);
