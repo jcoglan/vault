7d603fb11e57e5c72a87e30036bb0661ada819dc
James Coglan <jcoglan@gmail.com>
2013-01-15 12:49:26 +0000

    Separate remoteStorage client from Vault-specific logic.

--- node/remote_store.js
+++ node/remote_store.js
@@ -1,13 +1,10 @@
 var Vault = require('../lib/vault'),
-    http  = require('http'),
-    https = require('https'),
-    url   = require('url'),
-    qs    = require('querystring'),
-    oauth = require('remotestorage-oauth');
+    remoteStorage = require('./remotestorage');
 
 var RemoteStore = function() {
-  this._user = 'me';
-  this._host = 'local.dev';
+  this._client = new remoteStorage('getvau.lt', ['vault:rw']);
+  this._user   = 'me@local.dev';
+  this._conn   = this._client.connect(this._user, {browser: 'elinks', inline: true});
 };
 
 RemoteStore.prototype.export = function(callback, context) {
@@ -19,19 +16,11 @@ RemoteStore.prototype.import = function(json, callback, context) {
 };
 
 RemoteStore.prototype.listServices = function(callback, context) {
-  this._withAuth(function(error, root, token) {
+  this._conn.get('/vault/services/', function(error, listing) {
     if (error) return callback.call(context, error);
-
-    var uri     = url.parse(root),
-        path    = uri.pathname + '/vault/services/',
-        headers = {Authorization: 'Bearer ' + token};
-
-    this._request('GET', uri.host, path, {}, headers, function(error, response) {
-      if (error) return callback.call(context, error);
-      var services = Object.keys(JSON.parse(response.body.toString('utf8')));
-      callback.call(context, null, services);
-    });
-  }, this);
+    var services = Object.keys(JSON.parse(listing.content.toString('utf8')));
+    callback.call(context, null, services);
+  });
 };
 
 RemoteStore.prototype.saveGlobals = function(settings, callback, context) {
@@ -42,113 +31,40 @@ RemoteStore.prototype.saveService = function(service, settings, callback, contex
   this._save('/vault/services/' + service, settings, callback, context);
 };
 
-RemoteStore.prototype._save = function(path, settings, callback, context) {
-  this._withAuth(function(error, root, token) {
-    if (error) return callback.call(context, error);
-
-    var uri     = url.parse(root);
-        path    = uri.pathname + path,
-        headers = {Authorization: 'Bearer ' + token},
-        updated = {};
-
-    this._read(uri.host, path, headers, function(error, saved) {
-      if (error) return callback.call(cotnext, error);
-
-      Vault.extend(updated, settings);
-      Vault.extend(updated, saved);
-
-      var payload = JSON.stringify(updated, true, 2);
-      headers['Content-Type'] = 'application/json';
-
-      this._request('PUT', uri.host, path, payload, headers, callback, context);
+RemoteStore.prototype.serviceSettings = function(service, callback, context) {
+  this._read('/vault/global', function(error, global) {
+    this._read('/vault/services/' + service, function(error, local) {
+      Vault.extend(local, global);
+      callback.call(context, null, local);
     }, this);
   }, this);
 };
 
-RemoteStore.prototype._read = function(host, path, headers, callback, context) {
-  this._request('GET', host, path, {}, headers, function(error, response) {
+RemoteStore.prototype._save = function(path, settings, callback, context) {
+  this._read(path, function(error, saved) {
     if (error) return callback.call(context, error);
 
-    var payload = (response.statusCode === 200)
-                ? JSON.parse(response.body.toString('utf8'))
+    var updated = {};
+    Vault.extend(updated, settings);
+    Vault.extend(updated, saved);
+
+    var payload = JSON.stringify(updated, true, 2);
+
+    this._conn.put(path, 'application/json', payload, callback, context);
+  }, this);
+};
+
+RemoteStore.prototype._read = function(path, callback, context) {
+  this._conn.get(path, function(error, item) {
+    if (error) return callback.call(context, error);
+
+    var payload = item
+                ? JSON.parse(item.content.toString('utf8'))
                 : {};
 
     callback.call(context, null, payload);
   });
 };
-     
-RemoteStore.prototype.serviceSettings = function(service, callback, context) {
-  this._withAuth(function(error, root, token) {
-    var uri     = url.parse(root),
-        global  = uri.pathname + '/vault/global',
-        local   = uri.pathname + '/vault/services/' + service,
-        headers = {Authorization: 'Bearer ' + token};
-
-    this._read(uri.host, global, headers, function(error, globalSettings) {
-      this._read(uri.host, local, headers, function(error, localSettings) {
-        Vault.extend(localSettings, globalSettings);
-        callback.call(context, null, localSettings);
-      });
-    }, this);
-  }, this);
-};
-
-RemoteStore.prototype._withAuth = function(callback, context) {
-  if (this._token) return callback.call(context, null, this._root, this._token);
-
-  var resource = 'acct:' + this._user + '@' + this._host;
-
-  this._request('GET', this._host, '/.well-known/host-meta.json', {resource: resource}, {}, function(error, response) {
-    var jrd  = JSON.parse(response.body.toString('utf8')),
-        url  = jrd.links[0].properties['auth-endpoint'],
-        self = this;
-
-    oauth.authorize(url, 'Vault', ['vault:rw'], {}, function(error, token) {
-      self._root  = jrd.links[0].href;
-      self._token = token.access_token;
-      callback.call(context, null, self._root, self._token);
-    });
-  }, this);
-};
-
-RemoteStore.prototype._request = function(method, host, path, params, headers, callback, context) {
-  if (typeof params === 'string')  params = new Buffer(params, 'utf8');
-  if (!(params instanceof Buffer)) params = new Buffer(qs.stringify(params), 'utf8');
-
-  if (method === 'GET') path = path + '?' + params.toString();
-  if (method === 'PUT') headers['Content-Length'] = params.length;
-
-  var request = https.request({
-    method:   method,
-    host:     host,
-    path:     path,
-    headers:  headers
-  });
-
-  request.addListener('response', function(response) {
-    var chunks = [],
-        length = 0;
-
-    response.addListener('data', function(chunk) {
-      chunks.push(chunk);
-      length += chunk.length;
-    });
-    response.addListener('end', function() {
-      var buffer = new Buffer(length),
-          offset = 0;
-
-      for (var i = 0, n = chunks.length; i < n; i++) {
-        chunks[i].copy(buffer, offset);
-        offset += chunks[i].length;
-      }
-      response.body = buffer;
-      callback.call(context, null, response);
-    });
-  });
-
-  if (method === 'PUT') request.write(params);
-  request.end();
-};
-
+ 
 module.exports = RemoteStore;
 
---
+++ node/remotestorage.js
@@ -0,0 +1,206 @@
+var http  = require('http'),
+    https = require('https'),
+    url   = require('url'),
+    qs    = require('querystring'),
+    oauth = require('remotestorage-oauth');
+
+var UnauthorizedError = function() {};
+require('util').inherits(UnauthorizedError, Error);
+
+var remoteStorage = function(clientId, scopes, address) {
+  this._clientId = clientId;
+  this._scopes   = scopes;
+};
+
+remoteStorage.prototype.connect = function(address, options) {
+  return new Connection(this, address, options);
+};
+
+var Connection = function(client, address, options) {
+  var parts     = address.split('@');
+  this._client  = client;
+  this._address = address;
+  this._user    = parts[0];
+  this._host    = parts[1];
+  this._options = options || {};
+};
+
+Connection.prototype._storageDetails = function() {
+  return {
+    storage: this._storageUrl,
+    oauth:   this._oauthUrl,
+    token:   this._token
+  };
+};
+
+Connection.prototype.connect = function(callback, context) {
+  if (this._token) return callback.call(context, null, this._storageDetails());
+
+  this.discover(function(error, response) {
+    if (error) return callback.call(context, error);
+
+    this._storageUrl = response.storage;
+    this._oauthUrl   = response.oauth;
+
+    var url      = response.oauth,
+        clientId = this._client._clientId,
+        scopes   = this._client._scopes,
+        self     = this;
+
+    oauth.authorize(url, clientId, scopes, this._options, function(error, token) {
+      if (error) return callback.call(context, error);
+      self._token = token.access_token;
+      callback.call(context, null, self._storageDetails());
+    });
+  }, this);
+};
+
+Connection.prototype.discover = function(callback, context) {
+  var resource = 'acct:' + this._user + '@' + this._host,
+
+      urls = ['https', 'http'].map(function(scheme) {
+        return scheme + '://' + this._host + '/.well-known/host-meta.json';
+      }, this);
+
+  var attempt = function(index) {
+    var url = urls[index];
+    if (!url) return callback.call(context, new Error('Could not find remoteStorage endpoints for ' + this._address));
+
+    request('GET', url, {resource: resource}, {}, function(error, response) {
+      if (error) return attempt.call(this, index + 1);
+      var jrd, store, auth, self = this;
+
+      try {
+        jrd   = JSON.parse(response.body.toString('utf8'));
+        store = jrd.links[0].href;
+        auth  = jrd.links[0].properties['auth-endpoint'];
+      } catch (e) {
+        return attempt.call(this, index + 1);
+      }
+
+      callback.call(context, null, {storage: store, oauth: auth});
+    }, this);
+  };
+
+  attempt.call(this, 0);
+};
+
+Connection.prototype.get = function(path, callback, context) {
+  this.connect(function(error, response) {
+    if (error) return callback.call(context, error);
+
+    var url     = response.storage + path,
+        headers = {Authorization: 'Bearer ' + response.token};
+
+    request('GET', url, {}, headers, function(error, response) {
+      this._parseResponse(error, response, callback, context);
+    }, this);
+  }, this);
+};
+
+Connection.prototype.put = function(path, type, content, callback, context) {
+  this.connect(function(error, response) {
+    if (error) return callback.call(context, error);
+
+    var url     = response.storage + path,
+        headers = {
+          'Authorization': 'Bearer ' + response.token,
+          'Content-Type':  type
+        };
+
+    request('PUT', url, content, headers, function(error, response) {
+      this._parseResponse(error, response, callback, context);
+    }, this);
+  }, this);
+};
+
+Connection.prototype.delete = function(path, callback, context) {
+  this.connect(function(error, response) {
+    if (error) return callback.call(context, error);
+
+    var url     = response.storage + path,
+        headers = {Authorization: 'Bearer ' + response.token};
+
+    request('DELETE', url, {}, headers, function(error, response) {
+      this._parseResponse(error, response, callback, context);
+    }, this);
+  }, this);
+};
+
+Connection.prototype._parseResponse = function(error, response, callback, context) {
+  var status = response && response.statusCode;
+  if (status === 401 || status === 403)
+    error = new UnauthorizedError();
+
+  if (error) return callback.call(context, error);
+  if (status !== 200) return callback.call(context, null, null);
+
+  var content  = response.body,
+      type     = response.headers['content-type'],
+      etag     = response.headers['etag'],
+      modified = response.headers['last-modified'],
+
+      time     = etag     ? new Date(parseInt(etag, 10))
+               : modified ? new Date(modified)
+                          : null;
+
+  callback.call(context, null, {
+    content:  content,
+    type:     type,
+    version:  etag,
+    modified: time
+  });
+};
+
+var request = function(method, _url, params, headers, callback, context) {
+  var uri    = url.parse(_url),
+      client = (uri.protocol === 'https:') ? https : http,
+      path   = uri.path,
+      sep    = /\?/.test(path) ? '&' : '?';
+
+  if (typeof params === 'string')  params = new Buffer(params, 'utf8');
+  if (!(params instanceof Buffer)) params = new Buffer(qs.stringify(params), 'utf8');
+
+  if (method === 'GET') path = path + sep + params.toString();
+  if (method === 'PUT') headers['Content-Length'] = params.length;
+
+  var req = client.request({
+    method:   method,
+    host:     uri.hostname,
+    port:     uri.port || (client === https ? 443 : 80),
+    path:     path,
+    headers:  headers
+  });
+
+  req.addListener('response', function(response) {
+    var chunks = [],
+        length = 0;
+
+    response.addListener('data', function(chunk) {
+      chunks.push(chunk);
+      length += chunk.length;
+    });
+    response.addListener('end', function() {
+      var buffer = new Buffer(length),
+          offset = 0;
+
+      for (var i = 0, n = chunks.length; i < n; i++) {
+        chunks[i].copy(buffer, offset);
+        offset += chunks[i].length;
+      }
+      response.body = buffer;
+      callback.call(context, null, response);
+    });
+  });
+
+  req.addListener('error', function(error) {
+    callback.call(context, error);
+  });
+
+  if (method === 'PUT') req.write(params);
+  req.end();
+};
+
+module.exports = remoteStorage;
+remoteStorage.UnauthorizedError = UnauthorizedError;
+
