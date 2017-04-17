6669f4e951019e4ff658035b9c375355da84c858
James Coglan <jcoglan@gmail.com>
2017-04-17 16:27:06 +0100

    Replace the Vault.Stream class with the equivalent external package, sequin.

--- spec/node.js
+++ spec/node.js
@@ -1,7 +1,6 @@
 var jstest = require("jstest").Test
 
 require("./vault_spec")
-require("./stream_spec")
 // require("./node/generator_spec")
 // require("./node/config_file_spec")
 // require("./node/remote_sources_spec")
